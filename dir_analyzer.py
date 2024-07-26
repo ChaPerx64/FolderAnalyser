3e434704f22e98e6964fda3b8db370c05dafe4bc
ChaPerx64 <77404663+ChaPerx64@users.noreply.github.com>
2024-07-26 09:35:10 +0200

    feat: main functionality implemented

---
+++ dir_analyzer.py
@@ -0,0 +1,146 @@
+from typing import Annotated
+import os
+import typer
+from rich import print
+from rich.progress import Progress, SpinnerColumn, TextColumn, BarColumn, TimeRemainingColumn
+from rich.table import Table, Column
+import magic
+from dataclasses import dataclass
+from humanize import naturalsize
+
+searchable_types = {
+    "Image": {
+        "tag": "image/",
+    },
+    "Text": {
+        "tag": "text/",
+    },
+    "Audio": {
+        "tag": "audio/",
+    },
+    "Video": {
+        "tag": "video/",
+    },
+    "Application": {
+        "tag": "application/",
+    },
+}
+
+
+@dataclass
+class FiletypeInfoStorage:
+    tag: str
+    found_files = 0
+    found_size = 0
+    displayable_name: str
+
+
+result_storages = [
+    FiletypeInfoStorage(tag=value['tag'], displayable_name=name) for name, value in searchable_types.items()
+]
+others_storage = FiletypeInfoStorage(tag="None", displayable_name="Other")
+
+
+def main(
+    dir_path: Annotated[str, typer.Argument(help="Path to directory that needs to be analyzed")],
+    size_treshold: Annotated[float, typer.Option(
+        help="File size in GiB that gets the file marked")] = 1,
+    output_path: Annotated[str, typer.Option(
+        help="Path where reults will be output")] = "",
+):
+
+    if not os.path.exists(dir_path):
+        typer.secho("Incorrect path - does not exist", fg=typer.colors.RED)
+        raise typer.Exit()
+    if not os.path.isdir(dir_path):
+        typer.secho("Incorrect path - should be a directory",
+                    fg=typer.colors.RED)
+        raise typer.Exit()
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(
+            description=f"Counting files in `{dir_path}` for estimation...", total=None)
+        file_count = 0
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                file_count += 1
+    print(f"Preliminary file count: {file_count}")
+
+    with Progress(
+        SpinnerColumn(),
+        TimeRemainingColumn(),
+        BarColumn(),
+        TextColumn(
+            "[progress.description]{task.description}", table_column=Column()),
+        refresh_per_second=60,
+        speed_estimate_period=1,
+        transient=True,
+    ) as progress:
+        analyzed_files = 0
+        analysis_target = str(dir_path)
+        analysis_task_id = progress.add_task(
+            description=analysis_target, total=file_count, completed=analyzed_files
+        )
+        total_file_count = 0
+        total_file_size = 0
+        errored_files_count = 0
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                analyzed_files += 1
+                analysis_target = str(os.path.join(root, file))
+                counted = False
+                try:
+                    mime_type = magic.from_file(
+                        os.path.join(root, file), mime=True
+                    )
+                    file_size = os.path.getsize(analysis_target)
+                    total_file_size += file_size
+                    for storage in result_storages:
+                        if mime_type.startswith(storage.tag):
+                            storage.found_files += 1
+                            storage.found_size += file_size
+                            counted = True
+                    if not counted:
+                        others_storage.found_files += 1
+                        others_storage.found_size += file_size
+                    total_file_count += 1
+                except OSError:
+                    errored_files_count += 1
+                progress.update(
+                    analysis_task_id, description=analysis_target, completed=analyzed_files)
+
+    result_table = Table(title="Directory analysis results")
+    result_table.add_column("Media type")
+    result_table.add_column("Files found")
+    result_table.add_column("Size")
+    for storage in result_storages:
+        result_table.add_row(
+            storage.displayable_name,
+            str(storage.found_files),
+            str(naturalsize(storage.found_size)),
+        )
+    result_table.add_row(
+        others_storage.displayable_name,
+        str(others_storage.found_files),
+        str(naturalsize(others_storage.found_size)),
+    )
+    result_table.add_row(
+        "Errors",
+        str(errored_files_count),
+        "n/a",
+    )
+    result_table.add_section()
+    result_table.add_row(
+        "Totals",
+        str(total_file_count),
+        str(naturalsize(total_file_size)),
+    )
+    print(result_table)
+
+
+if __name__ == "__main__":
+    typer.run(main)
